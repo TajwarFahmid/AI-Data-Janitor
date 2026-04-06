f05828cff785fd6211ed76b80667b1b10909efa4
Tajwar Fahmid <123596299+TajwarFahmid@users.noreply.github.com>
2026-04-05 20:39:07 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# 1. Create the virtual environment using python3
+python3 -m venv venv
+
+# 2. Activate it (This is the Mac/Linux command)
+source venv/bin/activate
+
+# 3. Now install the tools (it won't ask for permission now!)
+pip install streamlit pandas groq python-dotenv
